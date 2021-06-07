548aafe3faf1ccbb67f1ffdbcb9c37667d50c0a4
aphrx <amalnnath20@yahoo.ca>
2021-06-07 15:41:58 -0700

    update thresholds and sell

--- crypto.py
+++ crypto.py
@@ -8,9 +8,23 @@ class Crypto:
     def fetch(self):
         resp = requests.get(f"https://api.kraken.com/0/public/Ticker?pair={self.crypto}")
         data = resp.json()
-        self.price['ask'] = data['result'][list(data['result'])[0]]['a'][0]
-        self.price['bid'] = data['result'][list(data['result'])[0]]['b'][0]
+        self.price['ask'] = float(data['result'][list(data['result'])[0]]['a'][0])
+        self.price['bid'] = float(data['result'][list(data['result'])[0]]['b'][0])
         return self.price
+
+    def buy(self, amount):
+        return amount/self.price['ask']
+
+    def sell(self, amount, wallet):
+        selling = amount/self.price['bid']
+        if selling <= wallet:
+            return selling, amount
+        elif wallet > 0:
+            return wallet, wallet * self.price['bid']
+        return 0, 0
+
+
+
 
 if __name__ == '__main__':
     btc = Crypto('BTCCAD')
--- trader.py
+++ trader.py
@@ -2,9 +2,14 @@ import time
 from crypto import Crypto
 
 class Trader:
-    def __init__(self, crypto="BTCCAD", capital=1000, interval=5):
+    def __init__(self, crypto="BTCCAD", capital=20000, interval=5):
         self.balance = capital
-        self.interval = 60
+        self.crypto_wallet = 0
+        self.interval = interval
+        self.buy_amount = 100 #How many dollars to buy when appropriate
+        self.sell_amount = 100
+        self.buy_threshold = 0
+        self.sell_threshold = 0
         self.crypto = Crypto(crypto)
         self.history = []
 
@@ -12,24 +17,47 @@ class Trader:
         while(True):
             res = self.crypto.fetch()
             self.history.append({'ask':res['ask'], 'bid': res['bid']})
-            print(res)
-            self.calc_average(15)
+            print('====================')
+            print('ask:', "${:.2f}".format(res['ask']))
+            print('bid:', "${:.2f}".format(res['bid']))
+            print('\n')
+            ask_avg, bid_avg = self.calc_average(15)
+            print('ask avg:', "${:.2f}".format(ask_avg))
+            print('bid avg:', "${:.2f}".format(bid_avg))
+            print('\n')
+            print('balance:', "${:.2f}".format(self.balance))
+            print('crypto wallet:', self.crypto_wallet)
+            print('portfolio value:', "${:.2f}".format(self.balance + (self.crypto_wallet * ask_avg)))
+            print('====================')
+
+            if len(self.history) > 5:
+                if res['ask'] > (ask_avg + self.buy_threshold) and self.balance > self.buy_amount:
+                    self.balance -= self.buy_amount
+                    bought = self.crypto.buy(self.buy_amount)
+                    self.crypto_wallet += bought
+                    print(f"\nBuying {bought} for ${self.buy_amount}")
+                elif res['bid'] < (bid_avg - self.sell_threshold):
+                    sold, amount = self.crypto.sell(self.sell_amount, self.crypto_wallet)
+                    if sold:
+                        self.crypto_wallet -= sold
+                        self.balance += amount
+                        print(f"\nSelling {sold} for ${amount}")
+
             time.sleep(self.interval)
             
     def calc_average(self, n):
         a = 0
         b = 0
-        print(self.history[-n:])
         for h in self.history[-n:]:
             a += float(h['ask'])
             b += float(h['bid'])
         
-        print(a/len(self.history[-n:]), b/len(self.history[-n:]))
+        return a/len(self.history[-n:]), b/len(self.history[-n:])
 
 
     def check(self):
         pass
 
 if __name__ == '__main__':
-    tdr = Trader()
+    tdr = Trader(interval=60)
     tdr.trade()
